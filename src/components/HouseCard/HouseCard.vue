5e92b846675c9cf58451bd85ed23291b66dedbbc
rczmm <3179623450@qq.com>
2025-02-04 04:44:16 +0800

    feat(myApp): 添加新组件和页面
    - 新增 HouseCard组件用于展示房源信息
    - 创建 collect、detail、message 和 my 页面
    - 添加相应的样式文件

---
+++ src/components/HouseCard/HouseCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <view class="room-list">
+    <view class="room-view" v-for="(data,index) in dataList" :key="index">
+      <image class="room-img"
+             :src="data.image" alt="11"></image>
+      <text>{{ data.name }}</text>
+      <view class="desc-view">
+        <text class="price-text">
+          $ {{ data.price }}
+        </text>
+        <text class="spec-text">
+          {{ data.roomNum }}室·{{ data.personNum }}人
+        </text>
+        <text class="rate-text">
+          <at-rate :max="1"/>
+          {{ data.rate }}
+        </text>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script setup lang="ts">
+import './index.css'
+
+defineProps({
+  dataList: {
+    type: Array,
+    default: () => []
+  }
+})
+
+</script>
---
+++ src/pages/detail/index.vue
@@ -0,0 +1,58 @@
+<template>
+
+  <view class="page-container">
+
+    <swiper
+      class='swiper-card'
+      indicatorColor='#999'
+      indicatorActiveColor='#333'
+      current="current"
+      :duration="500"
+      :interval="5000"
+      :circular="false"
+      :autoplay="true"
+      :indicatorDots="true">
+      <swiper-item v-for="(item, idx) in [
+        'https://img10.360buyimg.com/babel/s700x360_jfs/t25855/203/725883724/96703/5a598a0f/5b7a22e1Nfd6ba344.jpg!q90!cc_350x180',
+        'https://img11.360buyimg.com/babel/s700x360_jfs/t1/4776/39/2280/143162/5b9642a5E83bcda10/d93064343eb12276.jpg!q90!cc_350x180',
+        'https://img14.360buyimg.com/babel/s700x360_jfs/t1/4099/12/2578/101668/5b971b4bE65ae279d/89dd1764797acfd9.jpg!q90!cc_350x180',
+      ]" :key="idx">
+        <image :src="item" class="slide-image"/>
+      </swiper-item>
+    </swiper>
+
+    <view class="desc-card">
+      <text>t 43y8g134y789gvuyfe78gvergv78reg8rv7eqg7f83erg78</text>
+    </view>
+
+    <view class="house-card">
+      <image class="house-card-img"
+             src="https://i0.hdslb.com/bfs/archive/e2b8290c0d28855decb1e05b45d098689deb07e6.jpg@336w_190h_1c_!web-video-rcmd-cover.avif"></image>
+      <view class="details">
+        <text>全海景高品质大床房（马赛）</text>
+        <view class="tag-list">
+          <view class="tag">
+            <text>5.0</text>
+          </view>
+          <view class="tag">
+            <text>可做饭</text>
+          </view>
+        </view>
+        <view class="price-and-button">
+          <text class="price">￥880起</text>
+          <at-button type="primary" size="small">立即预订</at-button>
+        </view>
+      </view>
+    </view>
+
+
+  </view>
+
+
+</template>
+
+<script setup lang="ts">
+import './index.css'
+
+
+</script>
