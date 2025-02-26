5e6ea28ac32664c94deb42c4a140f544b172e478
rcz <3179623450@qq.com>
2025-02-26 16:46:01 +0800

    2311

--- src/pages/detail/index.vue
+++ src/pages/detail/index.vue
@@ -4,11 +4,7 @@
 
     <swiper class='swiper-card' indicatorColor='#999' indicatorActiveColor='#333' current="current" :duration="500"
       :interval="5000" :circular="false" :autoplay="true" :indicatorDots="true">
-      <swiper-item v-for="(item, idx) in [
-      'https://img10.360buyimg.com/babel/s700x360_jfs/t25855/203/725883724/96703/5a598a0f/5b7a22e1Nfd6ba344.jpg!q90!cc_350x180',
-      'https://img11.360buyimg.com/babel/s700x360_jfs/t1/4776/39/2280/143162/5b9642a5E83bcda10/d93064343eb12276.jpg!q90!cc_350x180',
-      'https://img14.360buyimg.com/babel/s700x360_jfs/t1/4099/12/2578/101668/5b971b4bE65ae279d/89dd1764797acfd9.jpg!q90!cc_350x180',
-    ]" :key="idx">
+      <swiper-item v-for="(item, idx) in images" :key="idx">
         <image :src="item" class="details-slide-image" />
       </swiper-item>
     </swiper>
